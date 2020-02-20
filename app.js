1ac2463893903624f4828851e46405626aecf022
Brian Nelson <nelsonsbrian@gmail.com>
2020-02-19 17:35:48 -0800

    add user session for authentication

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const session = required('express-session');
+const FileStore = require('session-file-store')(session);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -34,11 +36,42 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+  if (!req.session.user) {
+    const err = new Error(`You are not authenticated!`);
+    err.status = 401;
+    return next(err);
+  } else {
+    if (req.session.user === 'authenticated') {
+      return next();
+    } else {
+      const err = new Error(`You are not authenticated!`);
+      err.status = 401;
+      return next(err);
+    }
+  }
+
+}
+
+app.use(auth);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
