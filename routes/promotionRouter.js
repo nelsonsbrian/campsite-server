878ca3114fd3cca190830d59798c7a827479886e
Brian Nelson <nelsonsbrian@gmail.com>
2020-02-19 19:32:12 -0800

    week 3 workshop - add admin controls to updating and deleting content

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -18,7 +18,7 @@ promotionRouter.route('/')
       .catch(err => next(err));
   })
 
-  .post(authenticate.verifyUser, (req, res, next) => {
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.create(req.body)
       .then(promotion => {
         res.statusCode = 200;
@@ -34,7 +34,7 @@ promotionRouter.route('/')
     res.end('PUT operation not supported on /promotions');
   })
 
-  .delete(authenticate.verifyUser, (req, res, next) => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.deleteMany()
       .then(response => {
         res.statusCode = 200;
@@ -60,7 +60,7 @@ promotionRouter.route(`/:promotionId`)
     res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
   })
 
-  .put(authenticate.verifyUser, (req, res, next) => {
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.findByIdAndUpdate(req.params.promotionId, {
       $set: req.body
     }, { new: true })
@@ -72,7 +72,7 @@ promotionRouter.route(`/:promotionId`)
       .catch(err => next(err));
   })
 
-  .delete(authenticate.verifyUser, (req, res, next) => {
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.findByIdAndDelete(req.params.promotionId)
       .then(response => {
         res.statusCode = 200;
--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,14 @@ const passport = require('passport');
 const router = express.Router();
 const authenticate = require('../authenicate');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find()
+  .then(users => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+  })
+  .catch(err => next(err));
 });
 
 router.post('/signup', (req, res) => {
