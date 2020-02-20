bd9f59c48f35e3d2f8faf6e2ef4a9e578215dba3
Brian Nelson <nelsonsbrian@gmail.com>
2020-02-19 17:56:45 -0800

    add passport authentication with sessionIds

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const session = required('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenicate');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -46,22 +48,19 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  if (!req.session.user) {
+  if (!req.user) {
     const err = new Error(`You are not authenticated!`);
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === 'authenticated') {
-      return next();
-    } else {
-      const err = new Error(`You are not authenticated!`);
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 
 }
